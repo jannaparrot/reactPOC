5c80e9081ecde7bfc1428ad20db8eba780289726
jannaparrot <128072845+jannaparrot@users.noreply.github.com>
2025-06-21 18:15:05 -0400

    concepts working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
+import { useState } from 'react';
 import './App.css';
-import { Box, TextField } from '@mui/material';
+import { Box, TextField, Typography } from '@mui/material';
 
 function App() {
+  
+  const [sourceText, setSourceText] = useState<string>('');
+
+  // onInput expects an input that takes form event for an html element
+  const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+    // event has a target where the text comes from
+  const target = event.target as HTMLInputElement;
+    const text = target.value;
+    console.log(text)
+    const words = text.split(' ');
+    // instantiate variable is let (or var) for dynamic variable
+    let styledContent = '';
+    
+    //loop
+    words.forEach((word: string) => {
+      let color = 'black';
+      console.log(word.toLowerCase()==='them')
+      if (word.toLowerCase() === 'them') {
+        color = 'blue';
+      }
+      styledContent += `<span style="color: ${color};">${word}</span> `;
+    });
+
+    setSourceText(styledContent);
+  };
+
+
   return (
-    //A box is a div, but more.
+    <>
+    <TextField
+    onInput={handleInput}
+>
+
+</TextField>
     <Box>
-      Janna wuz here!!!!
-      <TextField>
-      
-      </TextField>
+      <Typography
+      suppressContentEditableWarning={true}
+      //fires event when typed
+      dangerouslySetInnerHTML={{ __html: sourceText }}
+    />
     </Box>
+    </>
   );
 }
 
